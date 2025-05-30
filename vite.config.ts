637a337c8a1c62e0801311141306c66fef3a7d93
B1teCode <ardasher.gaynazarov@mail.ru>
2025-05-30 17:30:12 +0500

    изменение vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
+  publicDir: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
